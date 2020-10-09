5a33f5d35baf961a93e2d49cd20621cf5e4289fa
thanadolps <thanadolps@gmail.com>
2020-10-10 01:05:47 +0700

    add file

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Welcome to _____ </h1>
+          <h5>____subtitle___</h5>
+        </div>
+        <div className="app-get">
+          <p>GET START</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
