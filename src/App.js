35c7711644e8e77b4a3fdf3500d5ac5e6c5b6cfd
thanadolps <thanadolps@gmail.com>
2020-10-30 22:38:58 +0700

    อันไหม่

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import padthai from './Pad-Thai-Food-Wallpaper-HD.jpeg'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1>Welcome to _____ </h1>
-          <h5>____subtitle___</h5>
-        </div>
-        <div className="app-get">
-          <p>GET START</p>
+        <div className="Head">
+          <img className="tex black" src={padthai} width={'100%'}></img>
+          <div className="tex">
+            <h1 className="center">SAWATDEEKUB</h1>
+          </div>
         </div>
       </div>
     );
